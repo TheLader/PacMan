3bd43c89e9206134a8dbd6702fd84611927e8450
TheLader <storozhuk.bogdan2015@gmail.com>
2024-02-06 15:04:15 +0200

    change class

--- Person.py
+++ Person.py
@@ -2,7 +2,7 @@ class Person:
     def __init__(self, name):
         self.name = name
     def SayMyName(self):
-        print(self.name)
+        print("моє імя" + self.name)
     def ChangeName(self, name):
         self.name = name
         print("Моє нове ім'я: " + name)
