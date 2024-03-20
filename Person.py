8a978bbca8f3d55b2833be3c1ef7475668ac407b
TheLader <storozhuk.bogdan2015@gmail.com>
2024-03-20 17:34:36 +0200

    Delete test class Person and create file Classes

--- Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
-    def SayMyName(self):
-        print("моє імя" + self.name)
-    def ChangeName(self, name):
-        self.name = name
-        print("Моє нове ім'я: " + name)
