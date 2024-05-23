2b7f5180fe145d19c25983b97e72517176f1be6f
TheLader <storozhuk.bogdan2015@gmail.com>
2024-05-23 17:13:37 +0300

    move tests

--- tests/test.py
+++ tests/test.py
@@ -8,42 +8,43 @@ pygame.init()
 pygame.display.set_mode((800, 600))
 
 # Тест ініціалізації гравця
+
 def test_player_initialization():
-    player = Classes.Player((7, 9), "Images\\PacMan.jpg", (50, 50))
+    player = Classes.Player((7, 9), "..\\Images\\PacMan.jpg", (50, 50))
     assert player.Position == (350, 450)
 
 # Тест ініціалізації ворогів
 def test_enemy_initialization():
-    enemy = Classes.Enemy((8, 5), "Images\\red_ghost.png", (50, 50))
+    enemy = Classes.Enemy((8, 5), "..\\Images\\red_ghost.png", (50, 50))
     assert enemy.Position == (400, 250)
 
 # Тест руху гравця
 def test_player_movement():
-    player = Classes.Player((7, 9), "Images\\PacMan.jpg", (50, 50))
+    player = Classes.Player((7, 9), "..\\Images\\PacMan.jpg", (50, 50))
     player.MovementDirection = (1, 0)
     player.Move()
     assert player.Position == (353, 450)
 
 # Тест зіткнення гравця з ворогом
 def test_player_enemy_collision():
-    player = Classes.Player((8, 9), "Images\\PacMan.jpg", (50, 50))
-    enemy = Classes.Enemy((7, 9), "Images\\red_ghost.png", (50, 50))
+    player = Classes.Player((8, 9), "..\\Images\\PacMan.jpg", (50, 50))
+    enemy = Classes.Enemy((7, 9), "..\\Images\\red_ghost.png", (50, 50))
     player.Move()
     enemy.Move()
     assert not enemy.ColliderRect.colliderect(player.ColliderRect)  # Перевіряємо, що немає зіткнення
-    player = Classes.Player((7, 9), "Images\\PacMan.jpg", (50, 50))
+    player = Classes.Player((7, 9), "..\\Images\\PacMan.jpg", (50, 50))
     player.Move()
     assert enemy.ColliderRect.colliderect(player.ColliderRect)
 
 
 # Тест збору їжі гравцем
 def test_player_food_collection():
-    player = Classes.Player((7, 9), "Images\\PacMan.jpg", (50, 50))
-    food = Classes.Food((7, 9), "images\\food.png", (50, 50))
+    player = Classes.Player((7, 9), "..\\Images\\PacMan.jpg", (50, 50))
+    food = Classes.Food((7, 9), "..\\images\\food.png", (50, 50))
     player.MovementDirection = (0,0)
     player.Move()
     assert is_player_center_in_point_center(player.ColliderRect, food.ColliderRect)  # Перевіряємо, що гравець зіткнувся з їжею
-    player = Classes.Player((8, 9), "Images\\PacMan.jpg", (50, 50))
+    player = Classes.Player((8, 9), "..\\Images\\PacMan.jpg", (50, 50))
     player.MovementDirection = (0, 0)
     player.Move()
     assert not is_player_center_in_point_center(player.ColliderRect, food.ColliderRect)
